2388faa00096e7e4918792066bd959346f75fbe8
mglssr <iglesiasmaia6@gmail.com>
2024-09-08 23:52:47 -0300

    clientesABB started

--- 2tarea/MaterialesTarea2/tarea2/src/clientesABB.cpp
+++ 2tarea/MaterialesTarea2/tarea2/src/clientesABB.cpp
@@ -2,25 +2,52 @@
 
 struct rep_clientesABB {
     TCliente cliente;
-    TClientesABB der;
-    TClientesABB izq;
+    TClientesABB derecho, izquierdo;
 };
-
 
 TClientesABB crearTClientesABBVacio(){
     return NULL;
 }
 
 void insertarTClienteTClientesABB(TClientesABB &clientesABB, TCliente cliente){
-
+    
+    if (clientesABB == NULL){
+        TClientesABB nuevo = new rep_clientesABB;
+        nuevo->cliente = cliente;
+        nuevo->derecho = nuevo->izquierdo = NULL;
+        clientesABB = nuevo;
+    }
+    else if(idTCliente(cliente) < idTCliente(clientesABB->cliente)){
+        insertarTClienteTClientesABB(clientesABB->izquierdo, cliente);
+    }
+    else if(idTCliente(cliente) > idTCliente(clientesABB->cliente)){
+        insertarTClienteTClientesABB(clientesABB->derecho, cliente);
+    }
+        //TClientesABB nuevo = new rep_clientesABB;
+        //nuevo->cliente = cliente;
 }
 
 void imprimirTClientesABB(TClientesABB clientesABB){
-
+    if (clientesABB != NULL){
+        imprimirTCliente(clientesABB->cliente);
+        imprimirTClientesABB(clientesABB->izquierdo);
+        imprimirTClientesABB(clientesABB->derecho);
+    }
 }
 
+void liberarnodo(TClientesABB nodo){
+    liberarTCliente(nodo->cliente);
+    delete(nodo);
+    nodo = NULL;
+}
+//hacer una funcion que libere un nodo de forma individual
 void liberarTClientesABB(TClientesABB &clientesABB){
-
+    if (clientesABB != NULL){
+        liberarTClientesABB(clientesABB->izquierdo);
+        liberarTClientesABB(clientesABB->derecho);
+        liberarnodo(clientesABB);
+    }
+    clientesABB = NULL;
 }
 
 bool existeTClienteTClientesABB(TClientesABB clientesABB, int idCliente){
@@ -32,7 +59,18 @@ TCliente obtenerTClienteTClientesABB(TClientesABB clientesABB, int idCliente){
 }
 
 nat alturaTClientesABB(TClientesABB clientesABB){
-    return 0;
+    nat alt_der, alt_izq;
+    if (clientesABB == NULL || (clientesABB->derecho == NULL && clientesABB->izquierdo == NULL)){
+        return 0;
+    }
+    alt_der = alturaTClientesABB(clientesABB->derecho);
+    alt_izq = alturaTClientesABB(clientesABB->izquierdo);
+    if (alt_der < alt_izq){
+        return (alt_izq + 1);
+    }
+    else{
+        return (alt_der + 1);
+    }
 }
 
 TCliente maxIdTClienteTClientesABB(TClientesABB clientesABB){
